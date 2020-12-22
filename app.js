80ded5dfd607d5d92088de67c7dc472f3772d9c9
Charli <charlispilecki@gmail.com>
2020-12-21 22:15:46 -0600

    Prompt user for info and use classes to create html

--- app.js
+++ app.js
@@ -33,3 +33,118 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+console.log("Please build your team")
+
+let employees = []
+
+getManagerInfo()
+
+function getManagerInfo() {
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is your manager's name?"
+        },
+        {
+            name: "id",
+            message: "What is your manager's id?"
+        },
+        {
+            name: "email",
+            message: "What is your manager's email?"
+        },
+        {
+            name: "officeNumber",
+            message: "What is your manager's office number?"
+        }
+        ])
+        .then(answers => {
+            let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+            employees.push(manager)
+            promptForTeamMember()
+        })
+}
+
+function promptForTeamMember() {
+    inquirer.prompt([
+        {
+            name: "teamMember",
+            type: "list",
+            message: "Which type of team member would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "I don't want to add anymore team members"
+            ]
+        }
+        ])
+        .then(answers => {
+            console.log(answers.teamMember)
+            if (answers.teamMember === 'Engineer') {
+                getEngineerInfo()
+            } else if (answers.teamMember === 'Intern') {
+                getInternInfo()
+            } else {
+                console.log('Creating HTML file')
+                let html = render(employees)
+                if (!fs.existsSync(OUTPUT_DIR)) {
+                    fs.mkdirSync(OUTPUT_DIR)
+                }
+                fs.writeFileSync(outputPath, html)
+            }
+        })
+}
+
+function getEngineerInfo() {
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is your engineer's name?"
+        },
+        {
+            name: "id",
+            message: "What is your engineer's id?"
+        },
+        {
+            name: "email",
+            message: "What is your engineer's email?"
+        },
+        {
+            name: "github",
+            message: "What is your engineer's github username?"
+        }
+        ])
+        .then(answers => {
+            let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+            employees.push(engineer)
+            promptForTeamMember()
+        })
+}
+
+function getInternInfo() {
+    inquirer.prompt([
+        {
+            name: "name",
+            message: "What is your intern's name?"
+        },
+        {
+            name: "id",
+            message: "What is your intern's id?"
+        },
+        {
+            name: "email",
+            message: "What is your intern's email?"
+        },
+        {
+            name: "school",
+            message: "What is your intern's school?"
+        }
+        ])
+        .then(answers => {
+            let intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+            employees.push(intern)
+            promptForTeamMember()
+        })
+
+}
